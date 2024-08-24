5870c2cc2361e788daec2946f3be0fe127aa581a
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-08-23 23:13:40 -0500

    feat: add View Transitions

--- src/data/books.js
+++ src/data/books.js
@@ -3,15 +3,21 @@ export const books = [
       id: "clean-code",
       title: "Clean Code: Manual de desarrollo ágil de software",
       img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+      opinion: "Este libro es una joya para cualquier programador. En él se recogen las mejores prácticas para escribir código limpio y fácil de mantener. Aunque está escrito en Java, los conceptos son aplicables a cualquier lenguaje de programación.",
+      author: "Robert C. Martin"
     },
     {
       id: 'the-clean-coder',
       title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
-      img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg'
+      img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+      opinion: "Este libro es una continuación de Clean Code. En él, Robert C. Martin nos enseña cómo ser un profesional de la programación. Aunque está escrito en Java, los conceptos son aplicables a cualquier lenguaje de programación.",
+      author: 'Robert C. Martin'
     },
     {
       id: 'javascript-the-good-parts',
       title: 'JavaScript: The Good Parts',
-      img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg'
+      img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+      opinion: "JavaScript es un lenguaje muy potente, pero también muy peligroso. En este libro, Douglas Crockford nos enseña las partes buenas del lenguaje y cómo evitar las malas.",
+      author: 'Douglas Crockford'
     }
   ];
